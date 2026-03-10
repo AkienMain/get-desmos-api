b1e72f5bd893e9e7b1cab9cdbe937b323d4baf31
akienmain <akienmain>
2026-03-10 01:51:54 +0100

    rate limiter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 export default {
 	async fetch(request, env, ctx) {
-		let url = 'https://www.desmos.com/api/v1.12/calculator.js?apiKey=' + env.DESMOS_APIKEY;
-		let response = await fetch(url);
-		return response;
+		const { pathname } = new URL(request.url)
+		const { success } = await env.RATE_LIMITER.limit({ key: pathname })
+		if (success) {
+			let url = 'https://www.desmos.com/api/v1.12/calculator.js?apiKey=' + env.DESMOS_APIKEY;
+			let response = await fetch(url);
+			return response;
+		} else {
+			return new Response(`429 Failure – rate limit exceeded for ${pathname}`, { status: 429 })
+		}
 	},
 };
